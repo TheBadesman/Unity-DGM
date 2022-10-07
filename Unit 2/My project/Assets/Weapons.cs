1e319197d2e415b322374934ed5fc08fc22bac2b
TheBadesman <Cameron@ottleys.net>
2022-10-06 18:26:21 -0600

    Weapons and Syntax
    Forgot to made a commit for the syntax, but here is the syntax and Name script, but I changed it a bit to be for a weapon instead. also added in a gun to the scene for the weapon script

---
+++ Unit 2/My project/Assets/Weapons.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+
+    public string weaponName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+      Debug.Log("My current weapon is a " + weaponName);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
