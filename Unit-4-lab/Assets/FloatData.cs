8fdda27e935433f40dff2ea023e125a56b27cc68
TheBadesman <Cameron@ottleys.net>
2022-10-12 23:08:35 -0600

    Lab Unit 4
    The data on the little health game of Unit 4's lab project, for videos 2 - the last one.

--- Unit-4-lab/Assets/FloatData.cs
+++ Unit-4-lab/Assets/FloatData.cs
@@ -6,4 +6,11 @@ public class FloatData : ScriptableObject {
 
     public float value;
 
+    public void UpdateValue(float num) 
+    {
+
+        value += num;
+
+    }
+
 }
--- Unit-4-lab/Assets/triggerEventBehavior.cs
+++ Unit-4-lab/Assets/triggerEventBehavior.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class triggerEventBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public UnityEvent triggerEnterEvent;
+
+    private void OnTriggerEnter(Collider other) {
+        triggerEnterEvent.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
