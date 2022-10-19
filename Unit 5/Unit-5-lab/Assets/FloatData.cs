8c863758b32e890ffc9a2297d6f30838f23a9faf
TheBadesman <Cameron@ottleys.net>
2022-10-19 17:35:02 -0600

    Lab 5 update
    Updated version of lab 5

--- Unit 5/Unit-5-lab/Assets/FloatData.cs
+++ Unit 5/Unit-5-lab/Assets/FloatData.cs
@@ -6,6 +6,13 @@ public class FloatData : ScriptableObject {
 
     public float value;
 
+    public void SetValue(float num)
+    {
+
+        value = num;
+
+    }
+
     public void UpdateValue(float num) 
     {
 
