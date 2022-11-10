7d16f1a65b7d8d78e36d2ad4feffbaaea5605f54
TheBadesman <Cameron@ottleys.net>
2022-11-09 23:19:25 -0700

    Unit 6.c lab work
    Work on the game manager for the unit 6.c lab

---
+++ 6a/UFO/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isGameOver;
+
+    private GameObject gameOverText;
+    //sets the time scale and tells the game that its not over
+    private void Awake()
+    {
+
+        Time.timeScale = 1;
+        isGameOver = false;    
+
+    }
+    // find the game over text
+    void Start()
+    {
+        
+        gameOverText = GameObject.Find("GameOverText");
+
+    }
+    //Checks to see if the game is over, and if it is it ends the game
+    void Update()
+    {
+        
+        if(isGameOver)
+        {
+            EndGame();
+        }
+        else
+        {
+            gameOverText.gameObject.SetActive(false);
+        }
+
+    }
+    //Stops time, and sets the game over text to active
+    public void EndGame()
+    {
+        gameOverText.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
